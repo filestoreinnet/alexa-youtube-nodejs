41a4e0c05c2f33f53da032a28afc050b8cd0d225
Mark Williams <mark.williams@diamond.ac.uk>
2019-08-13 11:14:09 +0100

    Look up URL from ytdl

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const Alexa = require('ask-sdk-core');
 const ytdl = require('ytdl-core');
+const testVideoID = '9bZkp7q19f0';
 
-let videoID='9bZkp7q19f0'
-ytdl.getInfo(videoID, (err, info) => {
-  if (err) throw err;
-  let format = ytdl.chooseFormat(info.formats, { quality: '140' });
-  if (format) {
-    console.log(format.url);
-  }
-});
+//For testing locally with node index.js
+if (require.main === module) {
+	getURLAndTitle(testVideoID,(url,title) => {
+		console.log(title);
+		console.log(url);
+	});
+}
+
+async function getURLAndTitle(videoID, callback) {
+    let info = await ytdl.getInfo(videoID);
+    let title = info.title
+    let format = ytdl.chooseFormat(info.formats, { quality: '140' });
+    if (format) {
+        let url = format.url;
+        callback(url, title);
+    }
+}
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
@@ -30,15 +40,19 @@ const SearchIntentHandler = {
             && handlerInput.requestEnvelope.request.intent.name === 'SearchIntent';
     },
     handle(handlerInput) {
-        const speechText='Playing some audio';
-        const playBehavior='REPLACE_ALL';
-        const url='https://d1le29qyzha1u4.cloudfront.net/AWS_Podcast_Episode_327.mp3';
-        const token='a';
-        const offset=0;
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .addAudioPlayerPlayDirective(playBehavior,url,token,offset)
-            .getResponse();
+        return new Promise((resolve) => {
+            getURLAndTitle(testVideoID,(url, title) => {
+                const speechText='Playing '+title;
+                const playBehavior='REPLACE_ALL';
+                const token='a';
+                const offset=0;
+                resolve(handlerInput.responseBuilder
+                    .speak(speechText)
+                    .addAudioPlayerPlayDirective(playBehavior,url,token,offset)
+                    .getResponse()
+                );
+            });
+        });
     }
 };
 const CancelAndStopIntentHandler = {

2ed717b1f8cf6a6c52acce15beb6dd01b2290565
Mark Williams <mark.williams@diamond.ac.uk>
2019-08-14 11:49:23 +0100

    Search youtube for videos

--- lambda/index.js
+++ lambda/index.js
@@ -1,25 +1,40 @@
 const Alexa = require('ask-sdk-core');
 const ytdl = require('ytdl-core');
-const testVideoID = '9bZkp7q19f0';
+const request = require('request');
 
-//import dev key
+
+// Insert Google dev key here, or in a file called DEVELOPER_KEY.js
 let DEVELOPER_KEY = '';
 try {
     DEVELOPER_KEY = require('./DEVELOPER_KEY.js');
 } catch(e) {
-    DEVELOPER_KEY = '';
+    // Ignore if file not there
 }
+
+const testVideoID = '9bZkp7q19f0';
+const testQuery = 'gangnam style'
 
 //For testing locally with node index.js
 if (require.main === module) {
-	getURLAndTitle(testVideoID,(url,title) => {
-		console.log(title);
-	});
+    //console.log(DEVELOPER_KEY);
+    //getURLAndTitle(testVideoID,(url,title) => {
+    //    console.log(title);
+    //});
+    youtubeSearch(testQuery, (id) => {
+        console.log(id)
+    });
+}
+
+async function youtubeSearch(query, callback) {
+    const url='https://www.googleapis.com/youtube/v3/search?part=id&q='+query+'&key='+DEVELOPER_KEY;
+    let id = await request(url, { json: true },  (err, res, body) => {
+        callback(res.body.items[0].id.videoId);
+    });
 }
 
 async function getURLAndTitle(videoID, callback) {
     let info = await ytdl.getInfo(videoID);
-    let title = DEVELOPER_KEY //info.title
+    let title = info.title
     let format = ytdl.chooseFormat(info.formats, { quality: '140' });
     if (format) {
         let url = format.url;
@@ -48,16 +63,18 @@ const SearchIntentHandler = {
     },
     handle(handlerInput) {
         return new Promise((resolve) => {
-            getURLAndTitle(testVideoID,(url, title) => {
-                const speechText='Playing '+title;
-                const playBehavior='REPLACE_ALL';
-                const token='a';
-                const offset=0;
-                resolve(handlerInput.responseBuilder
-                    .speak(speechText)
-                    .addAudioPlayerPlayDirective(playBehavior,url,token,offset)
-                    .getResponse()
-                );
+            youtubeSearch(testQuery, (id) => {
+                getURLAndTitle(id,(url, title) => {
+                    const speechText='Playing '+title;
+                    const playBehavior='REPLACE_ALL';
+                    const token='a';
+                    const offset=0;
+                    resolve(handlerInput.responseBuilder
+                        .speak(speechText)
+                        .addAudioPlayerPlayDirective(playBehavior,url,token,offset)
+                        .getResponse()
+                    );
+                });
             });
         });
     }
